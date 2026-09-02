5cf57402349155e3c4d1bfa2a04233c49a5db90f
aldi <aldisalam0756@gmail.com>
2026-09-02 20:13:43 +0800

    Memahami struktur dasar kode Java & menggunakan semua jenis Komentar

---
+++ Day001.java
@@ -0,0 +1,22 @@
+public class Day001{
+    public static void main(String[] args) {
+        /* ini cara menggunakan komentar panjangggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg
+        gggggggggggggggggggggggggggggggggggggggggggggg */
+
+        // ini contoh komentar satu baris
+
+        System.out.println("Ini commit hari pertama ku");
+    }
+}
