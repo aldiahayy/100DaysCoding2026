677369112c9ca36663a194fe34807f9b21cc8645
aldi <aldisalam0756@gmail.com>
2026-09-03 18:16:10 +0800

    Membuat Biodata Diri dengan Menggunakan println(), print() dan printf()

---
+++ Day002.java
@@ -0,0 +1,25 @@
+public class Day002 {
+    public static void main(String[] args) {
+        System.out.println("---- SOAL 1 ----");
+        System.out.println("Saya akan bertahan selama 100 Hari Ngoding, dan akan menjadi anggota resmi ISC");
+        System.out.println();
+        
+        System.out.println("---- SOAL 2 ----");
+        System.out.print("Selamat telah menjadi anggota ISC");
+        System.out.println("");
+        
+        System.out.println("---- SOAL 3 ----");
+        String nama = "Aldi Salam";
+        String nim = "D0224043";
+        char kelas = 'A';
+        int umur = 19;
+        double tinggiBadan = 164.5;
+        
+        System.out.printf("| %-5s : %-18s | %n", "Nama", nama);
+        System.out.printf("| %-5s : %-18s | %n", "NIM", nim);
+        System.out.printf("| %-5s : %-18c | %n", "Kelas", kelas);
+        System.out.printf("| %-5s : %-18d | %n", "Umur", umur);
+        System.out.printf("| %-5s : %-18.1f | %n", "TB", tinggiBadan);
+      
+    }
+}
