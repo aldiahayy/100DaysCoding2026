70c2f18b9d7cb224c1447a9fefa593af9df3e32e
aldi <aldisalam0756@gmail.com>
2026-09-04 22:50:43 +0800

    Penggunaan Escape Sequence (\n, \t, dan lain lain).
    berikut tugas hari ini:
    Buat sebuah program Java dengan tema "Profil Mahasiswa" yang menggunakan minimal 4 Escape Sequence.
    Minimal harus menggunakan:
    \n
    \t
    \"
    \\
    Program harus menghasilkan tampilan yang rapi dan mudah dibaca.
    Bonus: gunakan System.out.print() dan System.out.println() secara bersamaan untuk membuat tampilan yang berbeda.

---
+++ Day003.java
@@ -0,0 +1,30 @@
+public class Day003 {
+    public static void main(String[] args) {
+        // 1. \n : New Line
+        System.out.print("Hari ke 4 dari 100 hari\n");
+        System.out.println();
+        
+        // 2. \t : Horizontal Tab
+        System.out.print("Mantap\tbang\n\n");
+        
+        // 3. \\ :  Backslash
+        System.out.println("11\\September\\2025\n");
+        
+        // 4. \" : Double Quote
+        System.out.print("\"Jangan lupa bersyukur\"\n\n");
+        
+        // 5. \" : Single Quote
+        System.out.printf("%-7s \'Hebattt\'","Kata-kata :");
+        System.out.println("\n");
+        
+        // 6. \b : Backspace
+        System.out.println("diantara kau dan aku\b\b\n");
+        
+        // 7. \r : Cariage return
+        System.out.println("Aku kan\rMenghilang\n");
+        
+        // 8. Gabungannya bang
+        System.out.print("Kata-kata\t\'bang\'\n\"Ketakutan berasal dari ketidaktahuann\b\"\n");
+        System.out.println("Apakah itu benar? tidak atau \riya\\kah\\");
+ }
+}
