f0910d16920033fadc30a1b62b8a5946e3a2afe5
aldi <aldisalam0756@gmail.com>
2026-09-05 21:42:55 +0800

    Tipe Data Numerik Bilangan Bulat (byte, short, int, long).

---
+++ Day004.java
@@ -0,0 +1,13 @@
+public class Day004{
+  public static void main(String[]args){
+    byte umur = 20;
+    short tahun = 2006;
+    int saldo = 1050000;
+    long jarak = 200000000;
+    
+    System.out.println("umur saya\t\t: " + umur);
+    System.out.println("tahun lahir saya\t: " + tahun);
+    System.out.println("saldo teman saya\t: " + saldo);
+    System.out.println("jarak rumah ke kost\t: " + jarak);
+  }
+}
