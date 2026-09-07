4d024833fb0dc53256959f1c95ae470c18fb102f
aldi <aldisalam0756@gmail.com>
2026-09-07 15:35:39 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).
    LATIHAN HARI KE-6
    Buat program sederhana yang menggunakan char dan boolean.
    Output:
    =========================
        	DATA MAHASISWA
    =========================
    
    Nama   : Aldy
    Grade  : A
    Aktif  : true
    
    =========================

---
+++ Day006.java
@@ -0,0 +1,15 @@
+public class Day006{
+    public static void main(String[] args) {
+        String nama = "Aldi Salam";
+        char grade = 'A';
+        boolean aktif = true;
+
+        System.out.println("=========================");
+        System.out.println("     DATA MAHASISWA");
+        System.out.println("=========================\n");
+        System.out.println("Nama    : " + nama);
+        System.out.println("Grade   : " + grade);
+        System.out.println("Aktif   : " + aktif);
+        System.out.println("\n=========================");
+    }
+}
