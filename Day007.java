fd58dfe4914a14416ffc26b5ecaa50a7539e675a
aldi <aldisalam0756@gmail.com>
2026-09-08 21:48:16 +0700

    Pengenalan Tipe Data String untuk teks.
    LATIHAN HARI KE-7
    Buat program biodata sederhana menggunakan String.
    Output:
    =========================
         BIODATA MAHASISWA
    =========================
    
    Nama       : Aldy
    NIM        : 12345678
    Kelas      : TI-4A
    Jurusan    : Teknik Informatika
    Hobi       : Coding
    
    =========================

---
+++ Day007.java
@@ -0,0 +1,19 @@
+public class Day007{
+    public static void main(String[] args) {
+        String nama = "Aldi Salam";
+        String nim = "D0224043";
+        String kelas = "Informatika A 2024";
+        String jurusan = "Informatika";
+        String hobi = "Coding";
+
+        System.out.println("=========================");
+        System.out.println("    BIODATA MAHASISWA");
+        System.out.println("=========================");
+        System.out.println("Nama\t: " + nama);
+        System.out.println("NIM\t: " + nim);
+        System.out.println("Kelas\t: " + kelas);
+        System.out.println("Jurusan\t: " + jurusan);
+        System.out.println("Hobi\t: " + hobi);
+        System.out.println("\n=========================");
+    }
+}
