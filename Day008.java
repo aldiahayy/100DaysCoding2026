3c3680fc29065aa95796294a7d469b47b6d2052e
aldi <aldisalam0756@gmail.com>
2026-09-09 11:10:08 +0700

    Deklarasi, Inisialisasi dan Update Variabel.
    LATIHAN HARI KE-8
    Buat program yang mempunyai variabel:
    nama
    umur
    tinggi
    Kemudian:
    1.	Deklarasikan variabel. 
    2.	Berikan nilai awal. 
    3.	Ubah nilai umur. 
    4.	Tampilkan nilai sebelum dan sesudah di-update.

---
+++ Day008.java
@@ -0,0 +1,15 @@
+public class Day008 {
+    public static void main(String[] args) {
+        String nama;
+        int umur;
+        double tinggi;
+
+        nama = "Aldi Salam";
+        umur = 19;
+        tinggi = 165.5;
+
+        System.out.println("Halo nama saya " + nama + ", Umur saya itu " + umur + ", dan tinggi badan saya itu adalah " + tinggi + " cm.");
+        umur = 20;
+        System.out.println("Halo nama saya " + nama + ", Umur saya itu " + umur + ", dan tinggi badan saya itu adalah " + tinggi + " cm.");
+    }
+}
