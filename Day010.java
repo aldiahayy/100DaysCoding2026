d1e4e8795720848b6cf68d1150fa107d82c0465f
aldi <aldisalam0756@gmail.com>
2026-09-11 13:28:32 +0700

    Pemahaman Batas Minimal dan Maksimal dari tipe data
    LATIHAN HARI KE-10
    Buat program Java yang menampilkan nilai minimal dan maksimal dari:
    byte
    short
    int
    long
    float
    double
    char
    Gunakan konstanta seperti:
    Byte.MIN_VALUE
    Byte.MAX_VALUE
    Integer.MIN_VALUE
    Integer.MAX_VALUE
    dan konstanta tipe data lainnya.

---
+++ Day010.java
@@ -0,0 +1,35 @@
+public class Day010 {
+    public static void main(String[] args) {
+        System.out.println("byte");
+        System.out.println("Minimal  : " + Byte.MIN_VALUE);
+        System.out.println("Maksimal : " + Byte.MAX_VALUE);
+
+        System.out.println("\nshort");
+        System.out.println("Minimal  : " + Short.MIN_VALUE);
+        System.out.println("Maksimal : " + Short.MAX_VALUE);
+        
+        System.out.println("\nint");
+        System.out.println("Minimal  : " + Integer.MIN_VALUE);
+        System.out.println("Maksimal : " + Integer.MAX_VALUE);
+        
+        System.out.println("\nlong");
+        System.out.println("Minimal  : " + Long.MIN_VALUE);
+        System.out.println("Maksimal : " + Long.MAX_VALUE);
+        
+        System.out.println("\nlong");
+        System.out.println("Minimal  : " + Long.MIN_VALUE);
+        System.out.println("Maksimal : " + Long.MAX_VALUE);
+        
+        System.out.println("\nfloat");
+        System.out.println("Minimal  : " + Float.MIN_VALUE);
+        System.out.println("Maksimal : " + Float.MAX_VALUE);
+        
+        System.out.println("\ndouble");
+        System.out.println("Minimal  : " + Double.MIN_VALUE);
+        System.out.println("Maksimal : " + Double.MAX_VALUE);
+        
+        System.out.println("\nchar");
+        System.out.println("Minimal  : " + (int)Character.MIN_VALUE);
+        System.out.println("Maksimal : " + (int)Character.MAX_VALUE);
+    }
+}
