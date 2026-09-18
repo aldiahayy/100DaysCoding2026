bb24912b15c291ab09492019b65cb720839b8905
aldi <aldisalam0756@gmail.com>
2026-09-19 00:03:14 +0700

    Konversi Otomatis (Dari Tipe Data Kecil ke Besar)
    LATIHAN HARI KE-18
    Buatlah program Java yang memiliki sebuah variabel bertipe byte. Kemudian pindahkan nilai tersebut secara berurutan ke:
    short → int → long → float → double
    Tampilkan nilai dari setiap tipe data menggunakan System.out.println().

---
+++ Day018.java
@@ -0,0 +1,15 @@
+public class Day018 {
+    public static void main(String[] args) {
+        short nilaiShort = 100;
+        int nilaiInt = nilaiShort;
+        long nilaiLong = nilaiInt;
+        float nilaiFloat = nilaiLong;
+        double nilaiDouble = nilaiFloat;
+
+        System.out.println("Nilai short\t: " + nilaiShort);
+        System.out.println("Nilai int\t: " + nilaiInt);
+        System.out.println("Nilai long\t: " + nilaiLong);
+        System.out.println("Nilai float\t: " + nilaiFloat);
+        System.out.println("Nilai double\t: " + nilaiDouble);
+    }
+}
